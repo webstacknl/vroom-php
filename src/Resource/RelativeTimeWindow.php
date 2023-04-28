73bdb2c116b7441e557106fabfe62723d5080550
Marco Wansinck <marco@webstack.nl>
2023-04-28 09:28:28 +0200

    feat: Added constructor to RelativeTimeWindow

--- src/Resource/RelativeTimeWindow.php
+++ src/Resource/RelativeTimeWindow.php
@@ -9,4 +9,10 @@ final class RelativeTimeWindow implements TimeWindowInterface
     public int $start;
 
     public int $end;
+
+    public function __construct(int $start, int $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
 }
